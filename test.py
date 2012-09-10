22d3bf9b36d26d6bf4ff4041e664698b01825772
Larry Price <laprice@gmail.com>
2012-09-10 10:27:28 -0700

    created a rough and ready test suite to ensure that failures fail correctly.

---
+++ test.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from pushover import PushOverClient
+import sys
+
+def test_no_config():
+    try:
+        ps = PushOverClient(configfile="/dev/null")
+    except PushOverException:
+        assert(sys.exc_info()[1]=="No valid configuration found")    
+    return
+
+def test_message_too_big():
+    try:
+        ps = PushOverClient()
+        ps.send_message("""
+Whereas recognition of the inherent dignity and of the equal and inalienable rights of all members of the human family is the foundation of freedom, justice and peace in the world,
+
+Whereas disregard and contempt for human rights have resulted in barbarous acts which have outraged the conscience of mankind, and the advent of a world in which human beings shall enjoy freedom of speech and belief and freedom from fear and want has been proclaimed as the highest aspiration of the common people,
+
+Whereas it is essential, if man is not to be compelled to have recourse, as a last resort, to rebellion against tyranny and oppression, that human rights should be protected by the rule of law,
+
+Whereas it is essential to promote the development of friendly relations between nations,""")
+    except PushOverMessageTooBig:
+        assert(sys.exc_info()[1]=="The supplied message is bigger than 512 characters.")
+    return
+
+def test_send_message():
+    ps = PushOverClient()
+    ps.send_message("Test message from PushOverClient")
+
+if __name__=="__main__":
+    test_no_config()
+    test_message_too_big()
+    test_send_message()
+

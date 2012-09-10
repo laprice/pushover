44e5a0f6f92dc9ffd83a15e6a28f346112c4417f
Daniele Sluijters <github@daenney.net>
2012-09-10 16:52:59 +0200

    Add a console menu through OptParse.
    Though optparse is deprecated as per 2.7 is still works for every
    Python 2.x version so using that instead of argparse.
    
    Also redid all the quotes from single to doulbe and the spacing around
    the equal signs.

--- pushover.py
+++ pushover.py
@@ -34,32 +34,58 @@ class PushoverMessageTooBigException(PushoverException):
 
 class PushoverClient(object):
     """ PushoverClient, used to send messages to the Pushover.io service. """
-    def __init__(self, configfile=''):
+    def __init__(self, configfile=""):
         self.configfile = configfile
         self.parser = SafeConfigParser()
-        self.files = self.parser.read([self.configfile, os.path.expanduser('~/.pushover')])
+        self.files = self.parser.read([self.configfile, os.path.expanduser("~/.pushover")])
         if not self.files:
             logger.critical("No valid configuration found, exiting.")
             sys.exit(1)
-        self.conf = { 'app_key': self.parser.get('pushover','app_key'),
-             'user_key': self.parser.get('pushover','user_key')}
+        self.conf = { "app_key": self.parser.get("pushover","app_key"),
+             "user_key": self.parser.get("pushover","user_key")}
 
     def send_message(self, message):
         if len(message) > 512:
             raise PushoverMessageTooBigException("The supplied message is bigger than 512 characters.")
         payload = {
-                'token': self.conf['app_key'],
-                'user' : self.conf['user_key'],
-                'message': message,
+                "token": self.conf["app_key"],
+                "user" : self.conf["user_key"],
+                "message": message,
         }   
-        r = requests.post('https://api.pushover.net/1/messages.json', data=payload )
+        r = requests.post("https://api.pushover.net/1/messages.json", data=payload )
         if not r.status_code == requests.codes.ok:
             raise r.raise_for_status()
                       
-if __name__=='__main__':
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(module)s] %(levelname)s: %(message)s')
-    client = PushoverClient()
+if __name__=="__main__":
+    from optparse import OptionParser
+    parser = OptionParser()
+    parser.description = "This module will send a message through the Pushover.io notification service. It requires at least the '-m' / '--message' parameter to be passed."
+    parser.add_option("-c", "--config", dest = "configfile", help = "Location of the Pushover config file.")
+    parser.add_option("-d", "--debug", dest = "debug", action = "store_true", help = "Log at the DEBUG loglevel.")
+    parser.add_option("-m", "--message", dest = "message", help = "The message to send, will truncate to 512 chars.")
+    if len(sys.argv) <= 1:
+        parser.print_help()
+        sys.exit(1)
+    (options, args) = parser.parse_args()
+
+    if options.debug:
+        loglevel = logging.DEBUG
+    else:
+        loglevel = logging.INFO
+    logging.basicConfig(level=loglevel, format="%(asctime)s [%(module)s] %(levelname)s: %(message)s")
+
+    if options.configfile:
+        client = PushoverClient(options.configfile)
+    else:
+        client = PushoverClient()
+
+    if options.message:
+        options.message = options.message[:512]
+    else:
+        parser.error("Can't do anything without a message now can I?")
+
     try:
-        client.send_message("This is a test message")
+        client.send_message(options.message)
     except Exception as e:
         logger.critical("Something went wrong: {0}".format(e))
+

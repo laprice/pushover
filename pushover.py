7894cb0d20dabbd4aa30d559ace19ce3a3eddbd7
Daniele Sluijters <github@daenney.net>
2012-09-10 14:37:27 +0200

    Refactor this code into a class.
    We now have a PushoverClient class which can be initialised and
    passed and optional configfile, othrewise
    os.path.expanduser(~/.pushover) is used if available.

--- pushover.py
+++ pushover.py
@@ -3,7 +3,7 @@
 import sys
 import os
 import requests
-from ConfigParser import ConfigParser
+from ConfigParser import SafeConfigParser
 
 try:
     import simplejson as json
@@ -14,6 +14,7 @@ except ImportError:
         sys.exit("Please install the simplejson library or upgrade to Python 2.6+")
 
 import logging
+# Set up logging and add a NullHandler
 py27 = (2,7)
 cur_py = sys.version_info
 logger = logging.getLogger(__name__)
@@ -25,32 +26,33 @@ if cur_py < py27:
 else:
     logger.addHandler(logging.NullHandler())
 
-def find_config():
-    config = ConfigParser()
-    files = config.read(['.pushover',os.path.expanduser('~/.pushover')])
-    if not files:
-        logger.critical("No configuration found")
-        sys.exit(1)
-    conf = { 'app_key': config.get('pushover','app_key'),
+class PushoverClient(object):
+    """ PushoverClient, used to send messages to the Pushover.io service. """
+    def __init__(self, configfile=''):
+        self.configfile = configfile
+        self.parser = SafeConfigParser()
+        self.files = self.parser.read([self.configfile, os.path.expanduser('~/.pushover')])
+        if not self.files:
+            logger.critical("No valid configuration found, exiting.")
+            sys.exit(1)
+        self.conf = { 'app_key': config.get('pushover','app_key'),
              'user_key': config.get('pushover','user_key')}
-    return conf
 
-def send_message(conf,message):
-    # mild sanity check on the message
-    if len(message) > 512:
-        sys.exit("message too big")
-    payload = {
-            'token': conf['app_key'],
-            'user' : conf['user_key'],
-            'message': message,
-    }
-    r = requests.post('https://api.pushover.net/1/messages.json', data=payload )
-    if not r.status_code == requests.codes.ok:
-        logger.critical("Failed to send notification.")
-        logger.debug(r.headers)
+    def send_message(self, message):
+        if len(message) > 512:
+            sys.exit("message too big")
+        payload = {
+                'token': conf['app_key'],
+                'user' : conf['user_key'],
+                'message': message,
+        }   
+        r = requests.post('https://api.pushover.net/1/messages.json', data=payload )
+        if not r.status_code == requests.codes.ok:
+            logger.critical("Failed to send notification.")
+            logger.debug(r.headers)
                       
 if __name__=='__main__':
     logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(module)s] %(levelname)s: %(message)s')
-    c = find_config()
-    send_message(c,"This is a test message")
+    client = PushoverClient()
+    client.end_message("This is a test message")
     

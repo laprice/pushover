a2704c721bf6eefbf1e72787d951fbf7b7401e15
Larry Price <larry@industrialintellect.com>
2013-09-24 14:11:42 -0700

    tweaking package file so that it works in practice as well as in theory.

--- setup.py
+++ setup.py
@@ -2,16 +2,17 @@
 
 from setuptools import setup
 
-setup(name='pushover',
-      version='0.3',
-      description='Send messages via https://pushover.net/ to phones',
-      author='Larry Price, Daniele Sluijters, Josh Kaizoku',
-      author_email='laprice@gmail.com, github@daenney.net, kaizoku@phear.cc',
-      py_modules=['pushover'],
-      install_requires = ['Requests >= 0.14.0'],
-      license='BSD',
-      url='http://github.com/laprice/pushover',
-      entry_points={
-          'console_scripts': ['pushover = pushover.pushover:main']
-          }
+setup(
+    name='pushover',
+    version='0.3',
+    description='Send messages via https://pushover.net/ to phones',
+    author='Larry Price, Daniele Sluijters, Josh Kaizoku',
+    author_email='laprice@gmail.com, github@daenney.net, kaizoku@phear.cc',
+    py_modules=['pushover.pushover'],
+    install_requires = ['Requests >= 0.14.0'],
+    license='BSD',
+    url='http://github.com/laprice/pushover',
+    entry_points={
+        'console_scripts': ['pushover = pushover.pushover:main']
+    }
 )

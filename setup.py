585483a4117fd61098ebdc4af14b5720c15a7c22
Larry Price <laprice@gmail.com>
2024-06-24 16:11:06 -0700

    added authors set requests to latest version

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ setup(
     name='pushover',
     version='0.5',
     description='Send messages via https://pushover.net/ to phones',
-    author='Larry Price, Daniele Sluijters, Josh Kaizoku',
-    author_email='laprice@gmail.com, github@daenney.net, kaizoku@phear.cc',
+    author='Larry Price, Daniele Sluijters, Josh Kaizoku, Bryce Jasmer',
+    author_email='laprice@gmail.com, github@daenney.net, kaizoku@phear.cc, bryce@jasmer.com',
     py_modules=['pushover.pushover'],
-    install_requires = ['Requests >= 2.20.0'],
+    install_requires = ['Requests == 2.32.3'],
     license='BSD',
     url='http://github.com/laprice/pushover',
     entry_points={
